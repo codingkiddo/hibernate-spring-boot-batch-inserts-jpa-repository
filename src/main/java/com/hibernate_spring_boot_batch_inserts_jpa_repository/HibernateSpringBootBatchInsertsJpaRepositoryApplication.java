5b3e3cec8fc5bcf88c936f7e3a1d1af04b688bb0
codingkiddo <codingkiddo@gmail.com>
2025-10-06 11:11:00 +0530

    Initial Commit

---
+++ src/main/java/com/hibernate_spring_boot_batch_inserts_jpa_repository/HibernateSpringBootBatchInsertsJpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.hibernate_spring_boot_batch_inserts_jpa_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateSpringBootBatchInsertsJpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateSpringBootBatchInsertsJpaRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hibernate_spring_boot_batch_inserts_jpa_repository/HibernateSpringBootBatchInsertsJpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernate_spring_boot_batch_inserts_jpa_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateSpringBootBatchInsertsJpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
