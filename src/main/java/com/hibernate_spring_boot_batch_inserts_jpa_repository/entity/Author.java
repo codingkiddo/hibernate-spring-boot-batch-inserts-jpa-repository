01b1579da5dd28d8db619e06edcd1ab1eb32e900
codingkiddo <codingkiddo@gmail.com>
2025-10-06 11:24:54 +0530

    modified:   pom.xml 	modified:   src/main/java/com/hibernate_spring_boot_batch_inserts_jpa_repository/HibernateSpringBootBatchInsertsJpaRepositoryApplication.java 	new file:   src/main/java/com/hibernate_spring_boot_batch_inserts_jpa_repository/entity/Author.java 	new file:   src/main/java/com/hibernate_spring_boot_batch_inserts_jpa_repository/repository/AuthorRepository.java 	new file:   src/main/java/com/hibernate_spring_boot_batch_inserts_jpa_repository/service/BookstoreService.java 	modified:   src/main/resources/application.properties

---
+++ src/main/java/com/hibernate_spring_boot_batch_inserts_jpa_repository/entity/Author.java
@@ -0,0 +1,59 @@
+package com.hibernate_spring_boot_batch_inserts_jpa_repository.entity;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+
+@Entity
+public class Author implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id    
+    private Long id;
+
+    private String name;
+    private String genre;
+    private int age;
+    
+    @Version
+    private Short version;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }    
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Short getVersion() {
+        return version;
+    }   
+}
---
+++ src/main/java/com/hibernate_spring_boot_batch_inserts_jpa_repository/service/BookstoreService.java
@@ -0,0 +1,45 @@
+package com.hibernate_spring_boot_batch_inserts_jpa_repository.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.hibernate_spring_boot_batch_inserts_jpa_repository.entity.Author;
+import com.hibernate_spring_boot_batch_inserts_jpa_repository.repository.AuthorRepository;
+
+@Service
+public class BookstoreService {
+
+	@Value("${spring.jpa.properties.hibernate.jdbc.batch_size}")
+    private int batchSize;
+
+    private final AuthorRepository authorRepository;
+
+    public BookstoreService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+    
+    public void batchAuthors() {
+    	List<Author> authors = new ArrayList<>();
+    	for (int i = 0; i < 1000; i++) {
+            Author author = new Author();
+            author.setId((long) i + 1);
+            author.setName("Name_" + i);
+            author.setGenre("Genre_" + i);
+            author.setAge(18 + i);
+
+            authors.add(author);
+
+            if (i % batchSize == 0 && i > 0) {
+                authorRepository.saveAll(authors);
+                authors.clear();
+            }
+        }
+    	if (authors.size() > 0) {
+            authorRepository.saveAll(authors);
+            authors.clear();
+        }
+    }
+}
